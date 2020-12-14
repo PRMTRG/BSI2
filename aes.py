feccc26782a253be33d9e40393c2747adfdbc0cf
gottomy2 <igottomy@gmail.com>
2020-12-14 23:22:21 +0100

    Adding docstrings v2

--- aes.py
+++ aes.py
@@ -16,9 +16,9 @@ def encrypt(key_file, message_file, nonce_file, ciphertext_file, tag_file):
     """
     :param key_file: path to the file containing encryption key
     :param message_file: path to the message file which we want to encrypt
-    :param nonce_file:
+    :param nonce_file: path to (bytes, bytearray, memoryview) file
     :param ciphertext_file: path where the encrypted file shall be saved.
-    :param tag_file:
+    :param tag_file: path to message authentication code file
     :return: nothing
     """
     block_size = 16
@@ -36,9 +36,9 @@ def encrypt(key_file, message_file, nonce_file, ciphertext_file, tag_file):
 def decrypt(key_file, nonce_file, ciphertext_file, tag_file, output_file):
     """
     :param key_file: path to the file containing decryption key
-    :param nonce_file:
+    :param nonce_file: path to (bytes, bytearray, memoryview) file
     :param ciphertext_file: path to the file which we want to decrypt
-    :param tag_file:
+    :param tag_file: path to message authentication code file
     :param output_file: path where the decrypted file shall be saved.
     :return: nothing
     """
