af64119264fe0cba95f3c0908bc73616dfa89e84
TM <mirski.tymoteusz@gmail.com>
2021-01-17 00:49:59 +0100

    simple server with RSA encryption

---
+++ client.py
@@ -0,0 +1,62 @@
+"""
+Author:
+Tymoteusz Mirski
+"""
+
+
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+
+HOST = '127.0.0.1'
+PORT = 65432
+MAX_SIZE = 1024
+
+
+def encrypt(recipient_key, data):
+    session_key = get_random_bytes(16)
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_session_key = cipher_rsa.encrypt(session_key)   
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+    return enc_session_key + cipher_aes.nonce + tag + ciphertext
+
+
+def divide_and_send(s, data):
+    size = len(data)
+    while True:
+        if size > MAX_SIZE:
+            s.sendall(data[:MAX_SIZE])
+            s.sendall(b'0')
+            data = data[MAX_SIZE:]
+            size -= MAX_SIZE
+        else:
+            s.sendall(data)
+            s.sendall(b'1')
+            break
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        
+        # receive server's public key
+        data = s.recv(MAX_SIZE)
+        recipient_key = RSA.import_key(data.decode("utf-8"))
+        
+        while True:
+            message = input(">")
+            if message == "quit()":
+                break
+            message = message.encode("utf-8")
+            data = encrypt(recipient_key, message)
+            divide_and_send(s, data)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ server.py
@@ -0,0 +1,65 @@
+"""
+Author:
+Tymoteusz Mirski
+"""
+
+
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+
+HOST = '127.0.0.1'
+PORT = 65432
+MAX_SIZE = 1024
+
+
+def decrypt(private_key, data):
+    key_size = private_key.size_in_bytes()
+    enc_session_key, nonce, tag, ciphertext = \
+        data[:key_size], \
+        data[key_size:key_size+16], \
+        data[key_size+16:key_size+32], \
+        data[key_size+32:]
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    session_key = cipher_rsa.decrypt(enc_session_key)
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    plaintext = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    return plaintext.decode("utf-8")
+
+
+def main():
+    key = RSA.generate(2048)
+    private_key = key
+    public_key = key.publickey()
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connection from', addr)
+            print()
+            conn.sendall(public_key.export_key())
+            message = b''
+            while True:
+                data = conn.recv(MAX_SIZE)
+                if not data:
+                    break
+                message += data
+                over = conn.recv(1)
+                if not over:
+                    break
+                if over == b'1':
+                    print("Data received:")
+                    print(message)
+                    print()
+                    decrypted_message = decrypt(private_key, message)
+                    message = b''
+                    print("Decrypted message:")
+                    print(decrypted_message)
+                    print()
+                    
+            
+if __name__ == "__main__":
+    main()
